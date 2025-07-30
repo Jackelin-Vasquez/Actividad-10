fb1663e373a080a82b5b9337808adb7ae7f6ab50
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-29 23:25:53 -0600

    Diccionario

--- Actividad.py
+++ Actividad.py
@@ -1,11 +1,21 @@
 productos= {}
 
-numeros_productos= int(Input("Ingrese cantidad de productos a ingresar:"))
+numeros_productos= int(input("Ingrese cantidad de productos a ingresar:"))
 
 for  producto in range(numeros_productos):
     print(f"---Producto no.{producto +1}")
     codigo= input("Ingrese codigo de producto:")
-    nombre= input("Ingrese nombre de producto:")
-    categoria= input("Ingrese categoria:")
-    talla= input("Ingrese talla:")
+    nombre= input("Ingrese nombre de producto:").lower()
+    categoria= input("Ingrese categoria (Hombre, Mujer, Niño):").lower()
+    talla= input("Ingrese talla (S,M,L,XL) :").lower()
     Precio_unitario= float(input("Ingrese precio:"))
+    stock= int(input("Ingrese cantidad de stock de producto:"))
+
+    productos[codigo]= {
+        "Nombre":nombre,
+        "Categoria": categoria,
+        "talla":talla,
+        "Precio_Unitario": Precio_unitario,
+        "Stock":stock
+
+    }
